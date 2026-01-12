2edc4c210cfc97ea7b081c28f200e2fa859cddc6
BHIMAJIYANI ISHAAN KAUSHIKKUMAR <ishaanbhimaji@gmail.com>
2026-01-12 18:53:44 +0530

    added test file for test 8

---
+++ tests/test_8.py
@@ -0,0 +1,12 @@
+from fastapi.testclient import TestClient
+from fastapi import FastAPI,HTTPException
+app = FastAPI()
+client = TestClient(app)
+
+def test_report_structure():
+    run = client.post("/run", json={"suite": "default"})
+    run_id = run.json()["run_id"]
+
+    report = client.get(f"/report/{run_id}").json()
+    assert "results" in report
+    assert "summary" in report
