11f21c19bacca9daf42b2dddb7f4f1e493f06591
BHIMAJIYANI ISHAAN KAUSHIKKUMAR <ishaanbhimaji@gmail.com>
2026-01-14 22:32:00 +0530

    added comments

--- tests/test_1.py
+++ tests/test_1.py
@@ -1,6 +1,6 @@
 from app.loader import loader
 
 def test_1():
-    data = loader("test_cases.yaml")
-    assert "test_cases" in data
-    assert len(data["test_cases"]) == 10
+    data = loader("test_cases.yaml") #loads the file manually
+    assert "test_cases" in data #asserts test cases in the data
+    assert len(data["test_cases"]) == 10 #compares whether 10 cases were loaded or loaded
